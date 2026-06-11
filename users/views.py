df352e8b14eb5cb2efc58ea2e8fb781bda3d2dd4
Sarth4k <113513760+Sarth4k@users.noreply.github.com>
2026-06-11 17:44:15 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            auth_login(request, user)
             return redirect('task-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
-        print("IS VALID:", form.is_valid())
-        print("ERRORS:", form.errors)
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
